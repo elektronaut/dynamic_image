29cba096eb07abb620eeff71707cb51dd657aa70
Inge Jørgensen <inge@manualdesign.no>
2017-03-11 21:57:05 +0100

    Simpler magic byte handling

--- lib/dynamic_image/image_reader.rb
+++ lib/dynamic_image/image_reader.rb
@@ -13,11 +13,8 @@ module DynamicImage
 
     def valid_header?
       return false if file_header.blank?
-      magic_bytes.each do |expr|
-        if (expr.is_a?(Regexp) && file_header =~ /^#{expr}/) ||
-           (expr.is_a?(String) && file_header.start_with?(expr))
-          return true
-        end
+      magic_bytes.each do |str|
+        return true if file_header.start_with?(str)
       end
       false
     end
@@ -25,35 +22,16 @@ module DynamicImage
     private
 
     def file_header
-      @file_header ||= StringIO.new(@data, "rb").read(10)
-    end
-
-    def gif_magic_bytes
-      [
-        "\x47\x49\x46\x38\x37\x61".force_encoding("binary"),
-        "\x47\x49\x46\x38\x39\x61".force_encoding("binary")
-      ]
-    end
-
-    def jpeg_magic_bytes
-      [
-        "\xff\xd8".force_encoding("binary")
-      ]
+      @file_header ||= StringIO.new(@data, "rb").read(8)
     end
 
     def magic_bytes
-      gif_magic_bytes + png_magic_bytes + jpeg_magic_bytes + tiff_magic_bytes
-    end
-
-    def png_magic_bytes
       [
-        "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a".force_encoding("binary")
-      ]
-    end
-
-    def tiff_magic_bytes
-      [
-        "\x49\x49\x2a\x00".force_encoding("binary"),
+        "\x47\x49\x46\x38\x37\x61".force_encoding("binary"),         # GIF
+        "\x47\x49\x46\x38\x39\x61".force_encoding("binary"),
+        "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a".force_encoding("binary"), # PNG
+        "\xff\xd8".force_encoding("binary"),                         # JPEG
+        "\x49\x49\x2a\x00".force_encoding("binary"),                 # TIFF
         "\x4d\x4d\x00\x2a".force_encoding("binary")
       ]
     end
