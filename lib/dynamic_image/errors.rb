dbef3e14009e2f53269b43e5f6c5e756922325e9
Inge Jørgensen <inge@anyone.no>
2021-01-04 16:00:22 +0100

    Update Rubocop

--- lib/dynamic_image/errors.rb
+++ lib/dynamic_image/errors.rb
@@ -3,11 +3,17 @@
 module DynamicImage
   module Errors
     class Error < StandardError; end
+
     class InvalidImage < DynamicImage::Errors::Error; end
+
     class InvalidHeader < DynamicImage::Errors::Error; end
+
     class InvalidSignature < DynamicImage::Errors::Error; end
+
     class InvalidSizeOptions < DynamicImage::Errors::Error; end
+
     class InvalidTransformation < DynamicImage::Errors::Error; end
+
     class ParameterMissing < DynamicImage::Errors::Error; end
   end
 end
