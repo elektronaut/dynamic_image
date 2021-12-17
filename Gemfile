5db3657d4555bfdeec8dab7e81d3acca5ebabc4e
Inge Jørgensen <inge@elektronaut.no>
2021-12-17 17:02:38 +0100

    Test on Rails 7

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source "https://rubygems.org"
 
 gem "mime-types"
-gem "rails", "~> 6.1.0"
+gem "rails", "~> 7.0.0"
 
 gemspec
 
--- spec/internal/config/application.rb
+++ spec/internal/config/application.rb
@@ -19,6 +19,8 @@ module Internal
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.load_defaults 7.0
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
